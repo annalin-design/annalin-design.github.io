e3cc6a6ac3d6646efc5643ae114b3f01f2389501
annalin-design <30850473+annalin-design@users.noreply.github.com>
2021-10-09 10:54:10 -0400

    v1.0

---
+++ js/script.js
@@ -0,0 +1,30 @@
+$(function() {
+  $("header").load("../common/header.html");
+  $("footer").load("../common/footer.html");
+});
+
+
+
+$(function() {
+  var thehours = new Date().getHours();
+  var themessage;
+  var morning = ("Good morning &#127803;");
+  var afternoon = ("Good afternoon &#128075;");
+  var evening = ("Good evening &#10024;");
+  var midnight = ("You&rsquo;re still awake &#128064;");
+
+  if (thehours >= 0 && thehours < 4) {
+    themessage = midnight;
+
+  } else if (thehours >= 4 && thehours < 12) {
+    themessage = morning;
+
+  } else if (thehours >= 12 && thehours < 17) {
+    themessage = afternoon;
+
+  } else if (thehours >= 17 && thehours < 24) {
+    themessage = evening;
+  }
+
+  $(".greeting").append(themessage);
+});
