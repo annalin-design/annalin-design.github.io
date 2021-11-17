c2c9f9750fdf8939fb2dfbbe4d01a70e448337cd
annalin-design <30850473+annalin-design@users.noreply.github.com>
2021-11-16 21:57:58 -0500

    211116

--- js/script.js
+++ js/script.js
@@ -9,7 +9,7 @@ $(function() {
   var morning = ("Good morning &#127803;");
   var afternoon = ("Good afternoon &#128075;");
   var evening = ("Good evening &#10024;");
-  var midnight = ("You&rsquo;re still awake &#128064;");
+  var midnight = ("Good evening! You&rsquo;re still awake &#128064;");
 
   if (thehours >= 0 && thehours < 4) {
     themessage = midnight;
@@ -27,21 +27,24 @@ $(function() {
   $(".greeting").append(themessage);
 });
 
+
+
 $(document).ready(function() {
 
+  var $boxes = $('.grid-item');
+  $boxes.hide();
 
-// init Masonry
-var $grid = $('.grid-masonry').masonry({
-  itemSelector: '.grid-item',
-  columnWidth: '.grid-sizer',
-  gutter: '.gutter-sizer',
-  percentPosition: true,
-  //transitionDuration: 0
-});
-// layout Masonry after each image loads
-$grid.imagesLoaded().progress( function() {
-  $grid.masonry('layout');
-});
+  var $grid = $('.grid-masonry');
 
-});
+  $grid.imagesLoaded( function() {
+    $boxes.fadeIn(1000);
 
+    $grid.masonry({
+      itemSelector: '.grid-item',
+      columnWidth: '.grid-sizer',
+      gutter: '.gutter-sizer',
+      percentPosition: true,
+      
+    });    
+  });
+});
